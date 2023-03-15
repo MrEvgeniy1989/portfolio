93932feba8e85899ff7e8f63d67701da6f63cd9a
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-03-15 20:06:02 +0300

    Добавил иконки skills, которые можно менять в css как текст
    https://youtu.be/fe9CurY5Hk4
    
    https://docs.google.com/document/d/1EKj3YUF7p1tw8Oev_Lo61bKCD5GSq5aE6cndtmN0NQc/edit

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 
+import { faCss3Alt } from '@fortawesome/free-brands-svg-icons/faCss3Alt'
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons/faHtml5'
+import { faReact } from '@fortawesome/free-brands-svg-icons/faReact'
+
 import { Title } from '../common/components/title/Title'
 
-import cssIcon from './../assets/Icons/skills/css.svg'
-import formikIcon from './../assets/Icons/skills/formik.svg'
-import htmlIcon from './../assets/Icons/skills/html.svg'
-import javaScriptIcon from './../assets/Icons/skills/javaScript.svg'
-import reactIcon from './../assets/Icons/skills/react.svg'
-import reduxToolkitIcon from './../assets/Icons/skills/reduxToolkit.svg'
-import restApiIcon from './../assets/Icons/skills/restAPI.svg'
-import storyBookIcon from './../assets/Icons/skills/storyBook.svg'
-import typeScriptIcon from './../assets/Icons/skills/typeScript.svg'
-import unitTestsIcon from './../assets/Icons/skills/unitTest.svg'
 import { Skill } from './skill/Skill'
 import style from './Skills.module.scss'
 
@@ -22,66 +16,56 @@ export const Skills = () => {
         <Title text={'Skills'} />
         <div className={style.skills}>
           <Skill
-            imgSrc={typeScriptIcon}
-            imgAltTitle={''}
+            iconifyIcon={'mdi:language-typescript'}
             title={'TypeScript'}
             description={
               'Ab consequatur dolor dolorum eaque exercitationem incidunt ipsam maiores nobis nulla omnis praesentium...'
             }
           />
           <Skill
-            imgSrc={javaScriptIcon}
-            imgAltTitle={''}
+            iconifyIcon={'fa-brands:js'}
             title={'JavaScript'}
             description={
               'dolor dolorum eaque exercitationem incidunt ipsam maiores nobis nulla omnis praesentium...'
             }
           />
           <Skill
-            imgSrc={reactIcon}
-            imgAltTitle={''}
+            FontAwesomeIcon={faReact}
             title={'React'}
             description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
           />
           <Skill
-            imgSrc={reduxToolkitIcon}
-            imgAltTitle={''}
+            iconifyIcon={'tabler:brand-redux'}
+            title={'Redux'}
+            description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
+          />
+          <Skill
+            iconifyIcon={'tabler:brand-redux'}
             title={'Redux-Toolkit'}
             description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
           />
           <Skill
-            imgSrc={htmlIcon}
-            imgAltTitle={''}
+            FontAwesomeIcon={faHtml5}
             title={'HTML'}
             description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
           />
           <Skill
-            imgSrc={cssIcon}
-            imgAltTitle={''}
+            FontAwesomeIcon={faCss3Alt}
             title={'CSS'}
             description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
           />
           <Skill
-            imgSrc={formikIcon}
-            imgAltTitle={''}
-            title={'Formik'}
-            description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
-          />
-          <Skill
-            imgSrc={restApiIcon}
-            imgAltTitle={''}
+            iconifyIcon={'mdi:api'}
             title={'REST API'}
             description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
           />
           <Skill
-            imgSrc={storyBookIcon}
-            imgAltTitle={''}
+            iconifyIcon={'simple-icons:storybook'}
             title={'Story book'}
             description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
           />
           <Skill
-            imgSrc={unitTestsIcon}
-            imgAltTitle={''}
+            iconifyIcon={'file-icons:test-js'}
             title={'Unit tests'}
             description={'incidunt ipsam maiores nobis nulla omnis praesentium.'}
           />
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Icon } from '@iconify/react'
+
 import style from './Skill.module.scss'
 
 type SkillPropsType = {
   title: string
   description: string
-  imgSrc: string
-  imgAltTitle: string
+  imgSrc?: string
+  imgAltTitle?: string
+  FontAwesomeIcon?: IconProp
+  iconifyIcon?: string
 }
 
 export const Skill = (props: SkillPropsType) => {
+  let icon
+
+  if (props.FontAwesomeIcon) {
+    icon = <FontAwesomeIcon icon={props.FontAwesomeIcon} className={style.fontAwesomeIcon} />
+  } else if (props.iconifyIcon) {
+    icon = <Icon icon={props.iconifyIcon} className={style.iconifyIcon} fontSize={100} />
+  } else {
+    icon = <img src={props.imgSrc} alt={props.imgAltTitle} />
+  }
+
   return (
     <div className={style.skill}>
-      <div className={style.icon}>
-        <img src={props.imgSrc} alt={props.imgAltTitle} />
-      </div>
+      <div className={style.icon}>{icon}</div>
       <div className={style.skillInfo}>
         <h3>{props.title}</h3>
       </div>
