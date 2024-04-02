5abf2ab2a8746074c16022d03354ec2014fb1040
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-03 02:36:56 +0300

    refactor: Исправил рамку для фото.

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -29,23 +29,25 @@ export const Main = () => {
           </h1>
         </div>
 
-        {window.innerWidth > 1230 ? (
-          <Tilt
-            className={style.parallaxEffectImg}
-            gyroscope
-            perspective={800}
-            scale={1.1}
-            tiltMaxAngleX={40}
-            tiltMaxAngleY={40}
-            transitionSpeed={1500}
-          >
-            <img alt={'photo'} className={style.photo} src={photo} />
-          </Tilt>
-        ) : (
-          <div className={style.mobilePhotoBackground}>
-            <img alt={'photo'} className={style.mobilePhoto} src={photo} />
-          </div>
-        )}
+        <div className={style.photoFrame}>
+          {window.innerWidth > 1230 ? (
+            <Tilt
+              className={style.parallaxEffectImg}
+              gyroscope
+              perspective={800}
+              scale={1.1}
+              tiltMaxAngleX={40}
+              tiltMaxAngleY={40}
+              transitionSpeed={1500}
+            >
+              <img alt={'photo'} className={style.photo} src={photo} />
+            </Tilt>
+          ) : (
+            <div className={style.mobilePhotoBackground}>
+              <img alt={'photo'} className={style.mobilePhoto} src={photo} />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
