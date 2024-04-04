363cd7df061365567199866f7828ce7848b1a453
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-04 21:12:15 +0300

    feat: Переделал ссылки через map.

--- src/header/nav/Nav.tsx
+++ src/header/nav/Nav.tsx
@@ -2,13 +2,30 @@ import React from 'react'
 
 import style from './Nav.module.scss'
 
+const menuItems = [
+  { href: 'main', id: 1, title: 'Main' },
+  { href: 'skills', id: 2, title: 'Skills' },
+  { href: 'projects', id: 3, title: 'Projects' },
+  { href: 'contacts', id: 4, title: 'Contacts' },
+]
+
 export const Nav = () => {
   return (
-    <div className={style.nav}>
-      <a href={'#main'}>Main</a>
-      <a href={'#skills'}>Skills</a>
-      <a href={'#projects'}>Projects</a>
-      <a href={'#contacts'}>Contacts</a>
-    </div>
+    // <div className={style.nav}>
+    //   <a href={'#main'}>Main</a>
+    //   <a href={'#skills'}>Skills</a>
+    //   <a href={'#projects'}>Projects</a>
+    //   <a href={'#contacts'}>Contacts</a>
+    // </div>
+
+    <ul className={style.nav}>
+      {menuItems.map(item => {
+        return (
+          <a href={`#${item.href}`} key={item.id}>
+            {item.title}
+          </a>
+        )
+      })}
+    </ul>
   )
 }
