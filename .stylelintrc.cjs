4372aa8c4f0734a3b2b725dd420ebecec6c78cfe
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-04 15:48:41 +0300

    refactor: Удалил миксин flex.

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: '@it-incubator/stylelint-config',
   "rules": {
-    "scss/dollar-variable-pattern": null
+    "scss/dollar-variable-pattern": null,
+    "scss/at-mixin-pattern": null,
   }
 }
