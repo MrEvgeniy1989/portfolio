e94fb4434e02acc70d3efb03b7f87a95cac8f4e7
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-06 14:47:55 +0300

    feat: закоментировал Particle.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import s from './App.module.scss'
 
-import { Loader } from '../common/components/loader/Loader'
-import { Particle } from '../common/components/particle/Particle'
 import { ThemeProvider } from '../common/components/themeProvider/ThemeProvider'
 import { Footer } from '../features/footer/Footer'
 import { Header } from '../features/header/Header'
@@ -13,28 +11,28 @@ import { Skills } from '../features/skills/Skills'
 import { UpButton } from './upButton/UpButton'
 
 export const App = () => {
-  const [isParticleLoaded, setIsParticleLoaded] = useState(true)
-
-  useEffect(() => {
-    // Можно добавить любую логику, если необходимо, перед установкой isParticleLoaded в false
-    setIsParticleLoaded(false)
-  }, [])
+  // const [isParticleLoaded, setIsParticleLoaded] = useState(true)
+  //
+  // useEffect(() => {
+  //   // Можно добавить любую логику, если необходимо, перед установкой isParticleLoaded в false
+  //   setIsParticleLoaded(false)
+  // }, [])
 
   return (
     <div className={s.app}>
-      {isParticleLoaded ? (
-        <ThemeProvider>
-          <Particle init={isParticleLoaded} onLoaded={() => setIsParticleLoaded(true)} />
-          <Header />
-          <Main />
-          <Skills />
-          <Projects />
-          <Footer />
-          <UpButton />
-        </ThemeProvider>
-      ) : (
-        <Loader />
-      )}
+      {/*{isParticleLoaded ? (*/}
+      <ThemeProvider>
+        {/*<Particle init={isParticleLoaded} onLoaded={() => setIsParticleLoaded(true)} />*/}
+        <Header />
+        <Main />
+        <Skills />
+        <Projects />
+        <Footer />
+        <UpButton />
+      </ThemeProvider>
+      {/*) : (*/}
+      {/*  <Loader />*/}
+      {/*)}*/}
     </div>
   )
 }
