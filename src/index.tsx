9eb8212a4a54827677a533c94d7d56bb2da033dc
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-23 14:22:00 +0300

    refactor: обновил библиотеки с изменениями версии патч.

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,7 @@ import { createRoot } from 'react-dom/client'
 import '../src/common/styles/index.scss'
 
 import { App } from './app/App'
-import reportWebVitals from './reportWebVitals'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(<App />)
-
-reportWebVitals()
